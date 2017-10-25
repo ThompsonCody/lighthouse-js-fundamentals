80b1b61ea96505af37f92fe0b8d702846df686bd
Cody Thompson <c.williamthompson@gmail.com>
2017-10-25 10:07:24 +0000

    various JS learnings

---
+++ conditionals.js
@@ -0,0 +1,11 @@
+function whichSchool(age){
+  var school = (age < 13) ? "Elementary School" : (age >= 13 &&  age <= 18) ? "Secondary School" : "Lighthouse Labs";
+  return school;
+}
+
+console.log("I am 35. Which school should I go to?");
+console.log(whichSchool(35));
+console.log("I am 8. Which school should I go to?");
+console.log(whichSchool(8));
+console.log("I am 14. Which school should I go to?");
+console.log(whichSchool(14));
