85b14b134059a0692259b2f09870fb2beccbd0e2
Cody Thompson <c.williamthompson@gmail.com>
2017-10-25 11:24:15 +0000

    area

---
+++ area.js
@@ -0,0 +1,27 @@
+function calculateRectangleArea(l,w){
+  var result;
+  return result = ( l <= 0 || w <= 0 ) ? undefined : l * w;
+}
+
+function calculateTriangleArea(b,h){
+  var result;
+  return result = ( b <= 0 || h <= 0 ) ? undefined : b * h / 2;
+}
+
+function calculateCircleArea(x){
+  var area = Math.PI * Math.pow(x, 2),
+    result;
+  return result = ( area < 0 || x < 0 ) ? undefined : area;
+}
+console.log(calculateRectangleArea(10, 5));     // should print 50
+console.log(calculateRectangleArea(1.5, 2.5));  // should print 3.75
+console.log(calculateRectangleArea(10, -5));    // should print undefined
+
+console.log(calculateTriangleArea(10, 5)); // should print 25
+console.log(calculateTriangleArea(3, 2.5)); // should print 3.75
+console.log(calculateTriangleArea(10, -5)); // should print undefined
+
+console.log(calculateCircleArea(10)); // should print 314.159...
+console.log(calculateCircleArea(3.5)); // should print 38.484...
+console.log(calculateCircleArea(1.5)); // should print undefined
+"15" === 15
